26bbf6230f0cf22aecc2225b3545714d5ff132fb
solaJi <sola9191@gmail.com>
2024-04-10 22:21:11 +0900

    updated file_system.py added log.py

--- 2_desktop/10_messbox.py
+++
@@ -1,9 +0,0 @@
-import pyautogui
-
-# pyautogui.countdown(3)
-print("자동화 시작")
-# pyautogui.alert("자동화 수행에 실패했습니다.", "경고") # 확인 버튼만 있는 팝업
-# result = pyautogui.confirm("계속 진행하시겠습니까?", "확인") # 확인, 취소버튼
-# result = pyautogui.prompt('파일명을 무엇으로 하시겠습니까?', "입력") # 사용자 입력
-result = pyautogui.password("암호를 입력하세요") # 암호입력
-print(result)
--- 2_desktop/11_file_system.py
+++ 2_desktop/11_file_system.py
@@ -60,13 +60,48 @@ import os
 
 # 폴더 내에서 특정 패턴을 가진 파일들을 찾으려면?
 # *.xlsx, *.txt, 자동화*.png
-import fnmatch
-pattern = "1*.py"
-result = []
-for root, dirs, files in os.walk("."):
-    for name in files : 
-        if fnmatch.fnmatch(name , pattern) :
-            result.append(name)
+# import fnmatch
+# pattern = "1*.py"
+# result = []
+# for root, dirs, files in os.walk("."):
+#     for name in files : 
+#         if fnmatch.fnmatch(name , pattern) :
+#             result.append(name)
 
+# print(result)
 
-print(result)
+# 주어진 경로가 파일인지 폴더인지 확인
+# print(os.path.isdir("python")) # True
+# print(os.path.isfile("python")) # False
+
+# 지정된 경로에 해당하는 파일 / 폴더가 없다면 ?
+# print(os.path.isdir("pythons")) # False
+
+# # 주어진 경로가 존재하는지
+# if os.path.exists("python") : 
+#     print("파일 또는 폴더가 존재합니다.")
+# else : 
+#     print("존재하지 않습니다.")
+
+# 파일만들기
+# open("new_file.txt", "a").close() # 빈 파일 생성
+
+# 파일명 변경하기
+# os.rename("new_file.txt", "new_file_rename.txt") #new_file.txt를 new_file_rename.txt로 
+
+# 파일 삭제하기
+# os.remove("new_file_rename.txt")
+
+# 폴더 만들기
+# os.mkdir("new_folder") # 현재 경로 기준으로 폴더 생성
+# os.makedirs("new_folders/a/b/c") # 하위 폴더를 가지는 폴더 생성
+
+# 폴더명 변경하기
+# os.rename("new_folder", "new_folder_rename")
+
+# 폴더 지우기
+# os.rmdir("new_folder_rename") # 폴더 안이 비었을때만 삭제가능
+
+import shutil # shell utilities
+shutil.rmtree("new_folders") # 폴더 안이 비어있지 않아도 완전 삭제 가능
+# 모든 파일이 삭제될 수 있으므로 주의할것
